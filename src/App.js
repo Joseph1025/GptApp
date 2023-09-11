b875fa6efd73ef90a8a2d74f2021ec936fca4a31
Zexi Zhang <zhang.zexi@hotmail.com>
2023-09-11 11:35:37 -0400

    Reformatted code to react js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainPage from './components/MainPage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div className="App">Welcome!</div>
+        <MainPage />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage() {
+    const elements = [
+        <h1 key="header">Text here</h1>,
+    ];
+
+    return <div>{elements}</div>;
+}
+
+export default MainPage;
